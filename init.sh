edcba73b570246dada6e3b718253074c307003cb
Vasyl Gello <vasek.gello@gmail.com>
2018-06-11 00:09:52 +0300

    init.sh: add "rmbuildbranch" and "repopick_topic" aliases

--- init.sh
+++ init.sh
@@ -7,6 +7,25 @@
 
 # functions
 
+repopick_topic()
+{
+  # $1 is topic-name
+  # $2 is Gerrit URL
+
+  [ -z "$1" ] && echo "Usage: repopick_topic <topic-name> [gerrit-url]" && exit 1
+
+  # check if topic-name is not a numeric ID
+  echo "$1" | grep -ioe "[A-Za-z_-]" 1>/dev/null 2>/dev/null
+  [ $? -ne 0 ] && echo "topic-name must not be the numeric change ID" && exit 1
+
+  # perform repopick
+  if [ -z "$2" ]; then
+    repopick -s _"$1" -t "$1"
+  else
+    repopick -s _"$1" -t "$1" -g "$2"
+  fi
+}
+
 # start script
 
 if [[ "$(basename -- "$0")" == "init.sh" ]]; then
@@ -17,5 +36,5 @@ fi
 . build/envsetup.sh 1>/dev/null 2>/dev/null
 
 alias mkbuildbranch='repo abandon _build; repo forall -c "BRANCHES=\$(git branch | grep -ioe \" _[0-9A-Za-z_-]*$\"); [ \$? -eq 0 ] && repo start _build . && for BRANCH in \$BRANCHES; do git merge \$BRANCH; done"'
+alias rmbuildbranch='repo abandon _build'
 alias killshell='rm -f ~/.bash_history && kill -9 $(pidof $0)'
-
