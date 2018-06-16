64ae07325260c2e856ec6e7dc92601f90d64264b
Vasyl Gello <vasek.gello@gmail.com>
2018-06-16 23:39:56 +0300

    init.sh: add "fcb" alias for diffing binary files

--- init.sh
+++ init.sh
@@ -7,6 +7,26 @@
 
 # functions
 
+fcb()
+{
+  # $1 is file1
+  # $2 is file2
+
+  [ -z "$1" ] || [ -z "$2" ] && "Usage: fcb <file1> <file2>" && exit 1
+
+  cmp -l "$1" "$2" |
+    mawk 'function oct2dec(oct,    dec) {
+              for (i = 1; i <= length(oct); i++) {
+                  dec *= 8;
+                  dec += substr(oct, i, 1)
+              };
+              return dec
+          }
+          {
+              printf "%08X %02X %02X\n", $1-1, oct2dec($2), oct2dec($3)
+          }'
+}
+
 repopick_topic()
 {
   # $1 is topic-name
