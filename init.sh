6d644720b6ce763c42432ffb6e4a7752f8866aa7
Vasyl Gello <vasek.gello@gmail.com>
2018-09-09 11:29:04 +0300

    init.sh: Properly handle errors in helper functions

--- init.sh
+++ init.sh
@@ -20,7 +20,7 @@ gerrit_cr()
     [ "$LABEL" = "$3" ] && CR_LABEL="$LABEL" && break
   done
 
-  [ -z "$CR_LABEL" ] && echo "ERROR: Code-Review label must be \"-2\", \"-1\", \"0\", \"+1\", \"+2\" or \"n/a\" to skip the label" && exit 1
+  [ -z "$CR_LABEL" ] && echo "ERROR: Code-Review label must be \"-2\", \"-1\", \"0\", \"+1\", \"+2\" or \"n/a\" to skip the label" && return
 
   if [ "$CR_LABEL" = "n/a" ]
   then
@@ -35,7 +35,7 @@ gerrit_cr()
     [ "$LABEL" = "$4" ] && V_LABEL="$LABEL" && break
   done
 
-  [ -z "$V_LABEL" ] && echo "ERROR: Verified label must be \"-1\", \"0\", \"+1\" or \"n/a\" to skip the label" && exit 1
+  [ -z "$V_LABEL" ] && echo "ERROR: Verified label must be \"-1\", \"0\", \"+1\" or \"n/a\" to skip the label" && return
 
   if [ "$V_LABEL" = "n/a" ]
   then
@@ -58,7 +58,7 @@ fcb()
   # $1 is file1
   # $2 is file2
 
-  [ -z "$1" ] || [ -z "$2" ] && "Usage: fcb <file1> <file2>" && exit 1
+  [ -z "$1" ] || [ -z "$2" ] && "Usage: fcb <file1> <file2>" && return
 
   cmp -l "$1" "$2" |
     mawk 'function oct2dec(oct,    dec) {
@@ -78,11 +78,11 @@ repopick_topic()
   # $1 is topic-name
   # $2 is Gerrit URL
 
-  [ -z "$1" ] && echo "Usage: repopick_topic <topic-name> [gerrit-url]" && exit 1
+  [ -z "$1" ] && echo "Usage: repopick_topic <topic-name> [gerrit-url]" && return
 
   # check if topic-name is not a numeric ID
   echo "$1" | grep -ioe "[A-Za-z_-]" 1>/dev/null 2>/dev/null
-  [ $? -ne 0 ] && echo "topic-name must not be the numeric change ID" && exit 1
+  [ $? -ne 0 ] && echo "topic-name must not be the numeric change ID" && return
 
   # perform repopick
   if [ -z "$2" ]; then
