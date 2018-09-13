0f8666921cf9b60cc3c8956fb0c678782edfa9c9
Vasyl Gello <vasek.gello@gmail.com>
2018-09-13 15:26:50 +0300

    init.sh: Make functions from aliases

--- init.sh
+++ init.sh
@@ -351,6 +351,23 @@ repopick_topic()
   fi
 }
 
+mkbuildbranch()
+{
+  repo abandon _build
+  repo forall -c "BRANCHES=\$(git branch | grep -ioe \" _[0-9A-Za-z_-]*$\"); [ \$? -eq 0 ] && repo start _build . && for BRANCH in \$BRANCHES; do git merge \$BRANCH; done"
+}
+
+rmbuildbranch()
+{
+  repo abandon _build
+}
+
+killshell()
+{
+  rm -f ~/.bash_history
+  kill -9 $(pidof $0)
+}
+
 # start script
 
 if [[ "$(basename -- "$0")" == "init.sh" ]]; then
@@ -359,7 +376,3 @@ if [[ "$(basename -- "$0")" == "init.sh" ]]; then
 fi
 
 . build/envsetup.sh 1>/dev/null 2>/dev/null
-
-alias mkbuildbranch='repo abandon _build; repo forall -c "BRANCHES=\$(git branch | grep -ioe \" _[0-9A-Za-z_-]*$\"); [ \$? -eq 0 ] && repo start _build . && for BRANCH in \$BRANCHES; do git merge \$BRANCH; done"'
-alias rmbuildbranch='repo abandon _build'
-alias killshell='rm -f ~/.bash_history && kill -9 $(pidof $0)'
